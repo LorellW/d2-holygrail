667c1be252214cd9a0eb634dd5969de1ec6dd6f5
LorellW <lorellwendel@gmail.com>
2023-04-26 08:15:17 +0300

    Update SecurityConfig.java

--- src/main/java/com/github/lorellw/d2holygrail/config/SecurityConfig.java
+++ src/main/java/com/github/lorellw/d2holygrail/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.github.lorellw.d2holygrail.views.LoginView;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,9 +20,7 @@ public class SecurityConfig extends VaadinWebSecurity {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.rememberMe().alwaysRemember(true);
-
         super.configure(http);
-
         setLoginView(http, LoginView.class);
     }
 
