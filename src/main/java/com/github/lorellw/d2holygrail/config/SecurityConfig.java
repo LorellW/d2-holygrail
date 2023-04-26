acc087a176ae21af4dd547d3a6c10f650b0d7580
LorellW <lorellwendel@gmail.com>
2023-04-26 08:12:49 +0300

    4/26/2023
    configured logout

--- src/main/java/com/github/lorellw/d2holygrail/config/SecurityConfig.java
+++ src/main/java/com/github/lorellw/d2holygrail/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.github.lorellw.d2holygrail.config;
 import com.github.lorellw.d2holygrail.services.UserService;
 import com.github.lorellw.d2holygrail.views.LoginView;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,17 +13,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends VaadinWebSecurity {
-
-    private final UserService userService;
-    private final PasswordEncoder encoder;
-
-    public SecurityConfig(UserService userService, PasswordEncoder encoder) {
-        this.userService = userService;
-        this.encoder = encoder;
-    }
-
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private PasswordEncoder encoder;
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.rememberMe().alwaysRemember(true);
+
         super.configure(http);
 
         setLoginView(http, LoginView.class);
